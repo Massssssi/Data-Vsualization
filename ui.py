8195d821fb1edd06e708b3e17cb489724df1f9a4
Shreyas Honnalli <sc20sh@leeds.ac.uk>
2022-12-09 11:54:15 +0000

    datastrruture done

--- ui.py
+++ ui.py
@@ -1,4 +1,6 @@
 import plot
+import time
+import random
 #The following is the array of questions associated with its question number and its chart type. The array of these 3 objects will be shuffled and shown to user in randomised order
 #please update the third items with whatever map you intend to compare the bar with
 questions = [["What is the plastic pollution produced by the most polluting country?", 1, "bar"],
@@ -23,5 +25,22 @@ questions = [["What is the plastic pollution produced by the most polluting coun
             ["Which country spends the most money recycling plastic?", 10,"choro"],
 ]
 
-print(questions)
+random.shuffle(questions)
+
+scores = []
+
+#initialses a 3D list datastructure to keep track of time taken and whether user has got right for each question.
+#X axis are the 10 questions [0 to 9 possible values]
+#Y axis is whether bar or map for the specific question [2 possible values - 0 or 1. 0 represents bar, 1 represents map]
+#Z axis represents time or correctAnswer [2 possible values - 0 or 1]. 0 will be used to store time, 1 will be used to store whether user got it correct or not. 0 means he got it wrong, 1 means he got it correct.
+#Example to help understand better - if user attempts q1 barchart wrong and took 10 seconds to answer, the program will do scores[0][0][0] = 10.00, and scores[0][0][1] = 0
+for i in range(10):
+    scores.append([[0,0],[0,0]])
+
+print(scores)
+
 print("Welcome to our quiz, we will now be showing you 20 questions! try to answer these questions to the best of your ability!")
+input("Press Enter To Start")
+
+for i in range(20):
+    print("hi")
