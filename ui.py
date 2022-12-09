7ba8077088fb336046b676e1397e307fda5844ca
Shreyas Honnalli <sc20sh@leeds.ac.uk>
2022-12-09 12:12:30 +0000

    ready to merge to main

--- ui.py
+++ ui.py
@@ -25,15 +25,18 @@ questions = [["What is the plastic pollution produced by the most polluting coun
             ["Which country spends the most money recycling plastic?", 10,"choro"],
 ]
 
+#shuffles questions
 random.shuffle(questions)
 
-scores = []
+
+
 
 #initialses a 3D list datastructure to keep track of time taken and whether user has got right for each question.
 #X axis are the 10 questions [0 to 9 possible values]
 #Y axis is whether bar or map for the specific question [2 possible values - 0 or 1. 0 represents bar, 1 represents map]
 #Z axis represents time or correctAnswer [2 possible values - 0 or 1]. 0 will be used to store time, 1 will be used to store whether user got it correct or not. 0 means he got it wrong, 1 means he got it correct.
 #Example to help understand better - if user attempts q1 barchart wrong and took 10 seconds to answer, the program will do scores[0][0][0] = 10.00, and scores[0][0][1] = 0
+scores = []
 for i in range(10):
     scores.append([[0,0],[0,0]])
 
@@ -44,3 +47,4 @@ input("Press Enter To Start")
 
 for i in range(20):
     print("hi")
+    time.sleep(1)
